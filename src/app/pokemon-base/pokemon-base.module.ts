f5662394fd8f1d85c77996258ea6527a7df0aec8
Tung Dang <tungds@vimo.vn>
2023-06-26 18:44:11 +0700

    update pokemon module

---
+++ src/app/pokemon-base/pokemon-base.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokemonListComponent,
+    PokemonDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PokemonBaseModule { }
