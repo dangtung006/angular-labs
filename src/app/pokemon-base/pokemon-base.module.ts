32bb8d655f890a4b208061063bb4968a3e7961ad
Tung Dang <tungds@vimo.vn>
2023-06-26 22:31:10 +0700

    update base module

--- src/app/pokemon-base/pokemon-base.module.ts
+++ src/app/pokemon-base/pokemon-base.module.ts
@@ -6,23 +6,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
-
-
 @NgModule({
-  declarations: [
-    PokemonListComponent,
-    PokemonDetailComponent,
-  ],
-  imports: [
-    CommonModule,
-    HttpClientModule,
-  ],
-  exports: [
-    PokemonListComponent,
-    PokemonDetailComponent,
-  ],
-  providers: [
-  ],
-  bootstrap: []
+    declarations: [
+        PokemonListComponent,
+        PokemonDetailComponent,
+    ],
+    imports: [
+        CommonModule,
+        HttpClientModule,
+    ],
+    exports: [
+        PokemonListComponent,
+        PokemonDetailComponent,
+    ],
+    providers: [],
+    bootstrap: []
 })
 export class PokemonBaseModule { }
--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
-export class PokemonListComponent {
-
+export class PokemonListComponent implements OnInit {
+    pokemons : Pokemon[] = []
+    ngOnInit(): void {}
 }
